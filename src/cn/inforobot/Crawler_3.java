782498057f3b2c9d244dd51ce2776166dd0aa352
980635254 <980635254@qq.com>
2016-08-01 13:25:21 +0800

    change the classification()

--- src/cn/inforobot/Crawler_3.java
+++ src/cn/inforobot/Crawler_3.java
@@ -240,6 +240,50 @@ public class Crawler_3 {
 		return goods_doc;
 	}
 
+//	public static void classification() {
+//		Connection conn = DBUtil.getConn("info_rob");
+//		PreparedStatement ps = null;
+//		PreparedStatement ps1 = null;
+//		ResultSet rs = null;
+//		Set<String> set = new HashSet<String>();
+//		try {
+//			ps = conn.prepareStatement("");
+//			ps1 = conn.prepareStatement("");
+//			rs = ps.executeQuery("select distinct class from class_attribute");
+//			while (rs.next()) {
+//				set.add(rs.getString(1));
+//			}
+//			rs = ps.executeQuery("select class_data,url from goods where isnull(class_name) or class_name like '%(unclassfied)%' or class_name=''");
+//			while (rs.next()) {
+//				String s = rs.getString("class_data");
+//				String url = rs.getString("url");
+//				String flag = "";
+//				for (String x : set) {
+//					if (!x.contains("&")) {
+//						while (s.contains("&") && s.contains("›")) {
+//							s = s.substring(s.indexOf("›") + 1);
+//						}
+//					}
+//					if (s.contains(x)) {
+//						ps1.execute("update goods set class_name='" + x + "' where url='" + url + "'");
+//						flag = "Y";
+//						break;
+//					}
+//				}
+//				if (!flag.equals("Y")){
+//					while (s.contains("›")){
+//						s = s.substring(s.indexOf("›") + 1) + "(unclassfied)";
+//					}
+//					ps1.execute("update goods set class_name='" + s + "' where url='" + url + "'");
+//				}
+//			}
+//			ps1 = null;
+//			DBUtil.closeConn(rs, ps, conn);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			DBUtil.closeConn(rs, ps, conn);
+//		}
+//	}
 	public static void classification() {
 		Connection conn = DBUtil.getConn("info_rob");
 		PreparedStatement ps = null;
@@ -253,27 +297,37 @@ public class Crawler_3 {
 			while (rs.next()) {
 				set.add(rs.getString(1));
 			}
-			rs = ps.executeQuery("select class_data,url from goods where isnull(class_name) or class_name like '%(unclassfied)%' or class_name=''");
+			rs = ps.executeQuery("select class_data,url from goods where isnull(class_name) or class_name='' or class_name='unknown_class' or class_name like '%(unclassfied)%'");
 			while (rs.next()) {
 				String s = rs.getString("class_data");
 				String url = rs.getString("url");
 				String flag = "";
 				for (String x : set) {
+					String sc=s;
 					if (!x.contains("&")) {
-						while (s.contains("&") && s.contains("›")) {
-							s = s.substring(s.indexOf("›") + 1);
+						while (sc.contains("&") && sc.contains("›")) {
+							//sc = sc.substring(s.indexOf("›") + 1);
+							if(s.lastIndexOf("&")>s.lastIndexOf("›"))
+								sc="";
+							else
+							    sc = sc.substring(s.indexOf("›", s.lastIndexOf("&")) + 1);
 						}
 					}
-					if (s.contains(x)) {
+					if (sc.contains(x)) {
 						ps1.execute("update goods set class_name='" + x + "' where url='" + url + "'");
+						System.out.println(x);
 						flag = "Y";
 						break;
 					}
 				}
 				if (!flag.equals("Y")){
-					while (s.contains("›")){
-						s = s.substring(s.indexOf("›") + 1) + "(unclassfied)";
+					if(s.contains("›")){
+						s = s.substring(s.lastIndexOf("›") + 1) + "(undefined)";
 					}
+					while (s.startsWith(" ")){
+						s = s.replace(" ", "");
+					}
+					System.out.println(s);
 					ps1.execute("update goods set class_name='" + s + "' where url='" + url + "'");
 				}
 			}
@@ -284,4 +338,5 @@ public class Crawler_3 {
 			DBUtil.closeConn(rs, ps, conn);
 		}
 	}
+
 }
